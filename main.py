b390dfc40b515dd4033f34a178a9f8ba4a5028ad
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-06-27 22:29:23 +0500

    Send email using Simple Mail Transfer Protocol SMTP

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import smtplib
+
+my_email = "sender email goes here"
+my_password = "sender password goes here"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="receiving email goes here",
+        msg= "Subject: Hello\n\nThis is the body of the email."
+    )
